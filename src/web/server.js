e4545d7086b12662f3eafcc3dffc71e8128b6e7e
shaynlink <me@shaynlink.fr>
2020-06-01 18:24:30 +0200

    [fix] command server

--- src/web/server.js
+++ src/web/server.js
@@ -29,12 +29,12 @@ module.exports = function(client) {
     next();
   });
 
-  app.use((req, res, next) => {
+  app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers',
-        'X-Requested-With,content-type,Authorization');
+        'X-Requested-With, ontent-Type, Authorization');
     if ('OPTIONS' == req.method) {
       res.sendStatus(204);
     } else {
